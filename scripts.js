670be9f0714c26355d8d7165bb3c951d74a0d533
Carla <carla.dayane.berredo@gmail.com>
2022-04-18 07:00:52 -0400

    entrada na sala

--- scripts.js
+++ scripts.js
@@ -1,15 +1,28 @@
-function carregarMensagens(){
+let mensagens;
+let mensagem;
+entrar();
+function entrar(){
+    const nome = prompt("Qual seu nome?");
+    const obj = {name: nome};
+    const promisse = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants",obj)
+    promisse.then(carregarMensagens);
+    promisse.catch(tratarErro);
 
+}
+
+function tratarErro(erro){
+    console.log("Status code: " + erro.response.status);
+    alert("Insira outro nome, este já está em uso");
+    entrar();
+}
+
+function carregarMensagens(){
     const promisse = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
     console.log(promisse);
     promisse.then(renderizarMensagens);
-    /*setTimeout(function(){
-        window.location.reload(1);
-     }, 3000);*/
 }
 
-let mensagens;
-let mensagem;
+
 
 function renderizarMensagens(response){
     console.log(response.data);
@@ -21,7 +34,7 @@ function renderizarMensagens(response){
             mensagens.innerHTML += `<div class="fundo-mensagemSaiOuEntra">
                                         <p class="time-mensagem">(${mensagem.time})</p>
                                         <p class="from-mensagem">${mensagem.from}</p>
-                                        <p class="text-mensagem">${mensagem.text} ${i}</p>
+                                        <p class="text-mensagem">${mensagem.text}</p>
                                     </div>`
         }
         else if(mensagem.type == 'message' && mensagem.to === 'Reservadamente'){
@@ -46,4 +59,15 @@ function renderizarMensagens(response){
         }
        
     }
+    document.querySelector(".todas-mensagens").scrollIntoView(false);
+    setTimeout(function () {
+        mensagens="";
+        carregarMensagens(response);
+    }, 4000);
+    
+}
+function rolar(){
+    const elementoQueQueroQueApareca = document.querySelector('.mensagem');
+    elementoQueQueroQueApareca.scrollIntoView();
+    console.log(ultimaMensagem)
 }
